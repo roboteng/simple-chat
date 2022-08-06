2c41bbd23647bf990bbf9f2eb2272274a9f30973
Trevor Settles <roboteng@gmail.com>
2022-08-06 17:49:24 -0600

    added send button

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import userEvent from '@testing-library/user-event';
 
 test('Message textfield is cleared when the "Send" button is pressed', () => {
   render(<App />);
   const textbox = screen.getByRole("textbox");
-  // userEvent.type(textbox, "Hello, world!");
+  userEvent.type(textbox, "Hello, world!");
+  const send = screen.getByText(/send/i);
 });
