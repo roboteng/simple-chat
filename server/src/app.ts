f0c72a5bc297c9007918bd049477bc811649f20c
Trevor Settles <roboteng@gmail.com>
2022-08-07 12:49:52 -0600

    added post route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,15 @@ import express from "express";
 
 export function createApp(): express.Express {
   const app = express();
+  const messages: string[] = [];
 
   app.get("/api/messages", (req, res) => {
-    res.json([]);
+    res.json(messages.map(m => ({ body: m })));
   });
+
+  app.post("/api/messages", (req, res) => {
+    messages.push("Hello, world")
+    res.send();
+  })
   return app;
 }
--- server/src/server.test.ts
+++ server/src/server.test.ts
@@ -8,3 +8,16 @@ test("Responds with an empty list when nothing has happened yet", async () => {
     .send();
   expect(res.body).toEqual([]);
 })
+
+test("A message is sent back, once it is posted", async () => {
+  const app = createApp();
+  await supertest(app)
+    .post("/api/messages")
+    .send({
+      body: "Hello, world"
+    });
+  const res = await supertest(app)
+    .get("/api/messages")
+    .send();
+  expect(res.body).toEqual([{ body: "Hello, world" }]);
+});
