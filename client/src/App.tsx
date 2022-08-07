c13e6f564b00371b9b5df7c8497b5326491d08f7
Trevor Settles <roboteng@gmail.com>
2022-08-07 11:39:02 -0600

    refactored app into smaller components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,41 @@ import './App.css';
 
 function App() {
   const [message, setMessage] = useState("");
-  const [prevMessage, setPrevMessage] = useState<string[]>([]);
+  const [prevMessages, setPrevMessage] = useState<string[]>([]);
   const clearMessage = () => {
-    setPrevMessage([...prevMessage, message]);
+    setPrevMessage([...prevMessages, message]);
     setMessage("")
   };
   const saveMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value);
 
   return (
     <div className="App">
-      <textarea onChange={saveMessage} value={message} />
-      <button onClick={clearMessage}>Send</button>
-      <div>
-        <ul>
-          {prevMessage.map(m => <li>{m}</li>)}
-        </ul>
-      </div>
+      <MessageInput saveMessage={saveMessage} message={message} clearMessage={clearMessage} />
+      <MessageDisplay prevMessages={prevMessages} />
     </div>
   );
+}
+
+function MessageInput({ saveMessage, message, clearMessage }: {
+  saveMessage: (e: React.ChangeEvent<HTMLTextAreaElement>) => void,
+  message: string,
+  clearMessage: () => void,
+}) {
+  return <>
+    <textarea onChange={saveMessage} value={message} />
+    <button onClick={clearMessage}>Send</button>
+  </>
+}
+
+function MessageDisplay({ prevMessages }: {
+  prevMessages: string[]
+}) {
+  return <>
+    <div>
+      <ul>
+        {prevMessages.map(m => <li>{m}</li>)}
+      </ul>
+    </div></>
 }
 
 export default App;
